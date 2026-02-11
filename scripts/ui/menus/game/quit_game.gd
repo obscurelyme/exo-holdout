3adee1a5f8643736ba7ab90d163fed204504c75e
obscurelyme <nico.frozencoffee@gmail.com>
2026-02-10 20:34:25 -0500

    game to main scene nav

---
+++ scripts/ui/menus/game/quit_game.gd
@@ -0,0 +1,10 @@
+class_name QuitGameBtn
+extends Button
+
+var _main_scene_file: String = "res://scenes/main.tscn"
+
+func _ready() -> void:
+	button_up.connect(_handle_pressed)
+
+func _handle_pressed() -> void:
+	get_tree().change_scene_to_file(_main_scene_file)
