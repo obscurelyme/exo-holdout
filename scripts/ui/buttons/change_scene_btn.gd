e6717985e932861cd034f26bff67796e1f32844a
obscurelyme <nico.frozencoffee@gmail.com>
2026-02-11 19:42:48 -0500

    common change scene script

---
+++ scripts/ui/buttons/change_scene_btn.gd
@@ -0,0 +1,9 @@
+class_name ChangeSceneBtn
+extends Button
+
+@export var scene: Scenes.SceneTypes
+
+func _ready() -> void:
+	button_up.connect(_handle_pressed)
+func _handle_pressed() -> void:
+	get_tree().change_scene_to_file(Scenes.get_scene(scene))
